78b48edbfea62ffb1b57034422d84b7e29e7c6dc
Sedly <sedly.keith@student.moringaschool.com>
2024-04-11 12:04:46 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+
+  let newHeader;
+
+  before(() => {
+    newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    newHeader.innerHTML = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with the actual name
+    document.body.appendChild(newHeader);
+  });
+
+  
+      const mainNode = document.querySelector('main#main');
+      if (mainNode) {
+        mainNode.remove();
+      }
+      
+
+    
+
+   
